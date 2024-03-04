ab578c33c18b65ee0b82010ceef0d82e2be8a2fa
cadenpiper <120049430+cadenpiper@users.noreply.github.com>
2024-03-04 01:10:11 -0600

    Implimented token transfer functionality

--- test/TetraToken.js
+++ test/TetraToken.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-	let tToken, accounts, deployer
+	let tToken, accounts, deployer, receiver
 
 	beforeEach(async () => {
 		const TToken = await ethers.getContractFactory('TetraToken')
@@ -15,6 +15,7 @@ describe('Token', () => {
 
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
+		receiver = accounts[1]
 	})
 
 	describe('Deployment', () => {
@@ -44,4 +45,46 @@ describe('Token', () => {
 		})
 
 	})
+
+	describe('Sending Tokens', () => {
+		let amount, transaction, result
+
+		beforeEach(async () => {
+			amount = tokens(100)
+			transaction = await tToken.connect(deployer).transfer(receiver.address, amount)
+			result = await transaction.wait()
+		})
+
+		describe('Success', () => {
+			it('transfers token balances', async () => {
+				expect(await tToken.balanceOf(deployer.address)).to.equal(tokens(9900))
+				expect(await tToken.balanceOf(receiver.address)).to.equal(amount)
+			})
+
+			it('emits a transfer event', async () => {
+				const event = result.events[0]
+				expect(event.event).to.equal('Transfer')
+
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)
+			})
+		})
+
+		describe('Failure', () => {
+			it('rejects insufficient balances', async () => {
+				const invalidAmount = tokens(100000)
+				expect(await tToken.connect(deployer).transfer(receiver.address, amount)).to.be.reverted
+			})
+
+			it('rejects invalid recipient', async () => {
+				const amount = tokens(100)
+				expect(await tToken.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+			})
+
+		})
+
+	})
+
 })
